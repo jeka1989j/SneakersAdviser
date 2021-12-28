2bf097de49ad8d86b23dcc4ea850766dffb3d712
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2021-12-28 17:01:49 +0200

    update our MainActivity, which let the user choose value from spinner and return the result

--- app/src/main/java/com/example/sneakersadviser/MainActivity.kt
+++ app/src/main/java/com/example/sneakersadviser/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.sneakersadviser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chooseBrand = findViewById<Button>(R.id.choose_brand)
+        chooseBrand.setOnClickListener {
+            val brands = findViewById<TextView>(R.id.brands)
+            val spinnerBrands = findViewById<Spinner>(R.id.sneakers_brand_spinner)
+//            val setBrand = spinnerBrands.selectedItem.toString()
+            val setBrand = spinnerBrands.selectedItem.toString()
+            brands.text = "Sneakers brands is $setBrand"
+        }
     }
 }
